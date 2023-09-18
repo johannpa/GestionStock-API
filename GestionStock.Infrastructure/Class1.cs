21e5093ee9e229f83a4607f1784c09214050e13f
johannpa <johann_paque@hotmail.fr>
2023-09-18 15:30:36 +0200

    The biggining of the project

---
+++ GestionStock.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GestionStock.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
---
+++ GestionStock.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GestionStock.Service
+{
+    public class Class1
+    {
+    }
+}
